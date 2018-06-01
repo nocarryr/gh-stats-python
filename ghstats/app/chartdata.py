260d7bc198c2f9df62e2ea3ceb6ff69350b243ba
nocarryr <matt@nomadic-recording.com>
2018-06-01 11:24:16 -0500

    Move chart data generation into separate module

---
+++ ghstats/app/chartdata.py
@@ -0,0 +1,116 @@
+from ghstats import traffic
+from ghstats import utils
+from ghstats.app.colorutils import iter_colors
+
+async def get_repo_traffic_paths(app, context, repo_slug):
+    db_store = app['db_store']
+    coll = db_store.get_collection(traffic.TrafficPathEntry._collection_name)
+    filt = traffic.build_datetime_filter('datetime', **context)
+    filt['repo_slug'] = repo_slug
+    pipeline = [
+        {'$match':filt},
+        {'$group':{
+            '_id':'$path',
+            'count':{'$sum':'$count'},
+            'uniques':{'$sum':'$uniques'},
+            'path':{'$first':'$path'},
+            'title':{'$first':'$title'},
+        }},
+        {'$sort':{'count':-1}},
+    ]
+    async for doc in coll.aggregate(pipeline):
+        yield doc
+
+async def get_repos_by_rank(app, context, metric='count', limit=10):
+    repos = context['repos']
+    repo_slugs = context.get('repo_slugs')
+    db_store = app['db_store']
+    coll = db_store.get_collection(traffic.TrafficTimelineEntry._collection_name)
+    if not repo_slugs:
+        repo_slugs = [repo.repo_slug for repo in repos.values()]
+    pipeline = [
+        {'$match':{'repo_slug':{'$in':repo_slugs}}},
+        {'$group':{
+            '_id':'$repo_slug',
+            'total':{'$sum':'${}'.format(metric)},
+        }},
+        {'$sort':{'total':-1}},
+        {'$limit':limit},
+    ]
+    async for doc in coll.aggregate(pipeline):
+        yield doc
+
+async def get_timeline_for_repo(app, context, repo, metric):
+    db_store = app['db_store']
+    if isinstance(repo, traffic.Repo):
+        repo_slug = repo.repo_slug
+    else:
+        repo_slug = repo
+    coll = db_store.get_collection(traffic.TrafficTimelineEntry._collection_name)
+    filt = traffic.build_datetime_filter('timestamp', **context)
+    filt['repo_slug'] = repo_slug
+    pipeline = [
+        {'$match':filt},
+        {'$group':{
+            '_id':'$timestamp',
+            'value':{'$max':'${}'.format(metric)},
+        }},
+        {'$sort':{'_id':1}},
+    ]
+    async for doc in coll.aggregate(pipeline):
+        doc['timestamp'] = utils.make_aware(doc['_id'])
+        yield doc
+
+
+async def build_chart_datasets(app, context, metric, limit, hidden_repos):
+    all_dts = {}
+    color_iter = context.get('color_iter', iter_colors())
+    dataset_ids = []
+
+    async def build_chart_repo_dataset(repo_doc):
+        repo_slug = repo_doc['_id']
+        dataset_ids.append(repo_slug)
+        async def build_rows():
+            async for tl_doc in get_timeline_for_repo(app, context, repo_slug, metric):
+                dt = tl_doc['timestamp']
+                dt_str = all_dts.get(dt)
+                if dt_str is None:
+                    dt_str = utils.dt_to_str(dt)
+                    all_dts[dt] = dt_str
+                yield {'t':dt_str, 'y':tl_doc['value']}
+
+        color = next(color_iter)
+        tdata = {
+            'label':'{} Total'.format(repo_slug),
+            'fill':False,
+            'backgroundColor':color,
+            'borderColor':color,
+            'lineTension':0,
+            'spanGaps':True,
+            'hidden':repo_slug in hidden_repos,
+        }
+        if metric == 'count':
+            tdata['label'] = '{} Total'.format(repo_slug)
+        elif metric == 'uniques':
+            tdata['label'] = '{} Uniques'.format(repo_slug)
+        tdata['data'] = [d async for d in build_rows()]
+        return tdata
+
+    repo_iter = get_repos_by_rank(app, context, metric, limit)
+    datasets = [await build_chart_repo_dataset(repo_doc) async for repo_doc in repo_iter]
+    start_dt = min(all_dts.keys())
+    data = {
+        'chart_data':{
+            'datasets':datasets,
+        },
+        'dataset_ids':dataset_ids,
+        'start_datetime':all_dts[start_dt],
+    }
+    return data
+
+async def get_traffic_chart_data(app, context):
+    repos = context['repos']
+    hidden_repos = context['hidden_repos']
+    metric = context.get('data_metric', 'count')
+    limit = context.get('limit', 10)
+    return await build_chart_datasets(app, context, metric, limit, hidden_repos)
--- ghstats/app/main.py
+++ ghstats/app/main.py
@@ -13,6 +13,7 @@ from ghstats.dbstore import DbStore
 from ghstats import utils
 from ghstats.app.colorutils import iter_colors
 from ghstats.app import templatetags
+from ghstats.app import chartdata
 
 BASE_PATH = os.path.abspath(os.path.dirname(__file__))
 STATIC_ROOT = os.path.join(BASE_PATH, 'static')
@@ -90,118 +91,6 @@ def update_context_dt_range(request, context=None):
         context['end_datetime_str'] = utils.dt_to_str(now)
     return context
 
-async def get_repo_traffic_paths(app, context, repo_slug):
-    db_store = app['db_store']
-    coll = db_store.get_collection(traffic.TrafficPathEntry._collection_name)
-    filt = traffic.build_datetime_filter('datetime', **context)
-    filt['repo_slug'] = repo_slug
-    pipeline = [
-        {'$match':filt},
-        {'$group':{
-            '_id':'$path',
-            'count':{'$sum':'$count'},
-            'uniques':{'$sum':'$uniques'},
-            'path':{'$first':'$path'},
-            'title':{'$first':'$title'},
-        }},
-        {'$sort':{'count':-1}},
-    ]
-    async for doc in coll.aggregate(pipeline):
-        yield doc
-
-async def get_repos_by_rank(app, context, metric='count', limit=10):
-    repos = context['repos']
-    repo_slugs = context.get('repo_slugs')
-    db_store = app['db_store']
-    coll = db_store.get_collection(traffic.TrafficTimelineEntry._collection_name)
-    if not repo_slugs:
-        repo_slugs = [repo.repo_slug for repo in repos.values()]
-    pipeline = [
-        {'$match':{'repo_slug':{'$in':repo_slugs}}},
-        {'$group':{
-            '_id':'$repo_slug',
-            'total':{'$sum':'${}'.format(metric)},
-        }},
-        {'$sort':{'total':-1}},
-        {'$limit':limit},
-    ]
-    async for doc in coll.aggregate(pipeline):
-        yield doc
-
-async def get_timeline_for_repo(app, context, repo, metric):
-    db_store = app['db_store']
-    if isinstance(repo, traffic.Repo):
-        repo_slug = repo.repo_slug
-    else:
-        repo_slug = repo
-    coll = db_store.get_collection(traffic.TrafficTimelineEntry._collection_name)
-    filt = traffic.build_datetime_filter('timestamp', **context)
-    filt['repo_slug'] = repo_slug
-    pipeline = [
-        {'$match':filt},
-        {'$group':{
-            '_id':'$timestamp',
-            'value':{'$max':'${}'.format(metric)},
-        }},
-        {'$sort':{'_id':1}},
-    ]
-    async for doc in coll.aggregate(pipeline):
-        doc['timestamp'] = utils.make_aware(doc['_id'])
-        yield doc
-
-
-async def build_chart_datasets(app, context, metric, limit, hidden_repos):
-    all_dts = {}
-    color_iter = context.get('color_iter', iter_colors())
-    dataset_ids = []
-
-    async def build_chart_repo_dataset(repo_doc):
-        repo_slug = repo_doc['_id']
-        dataset_ids.append(repo_slug)
-        async def build_rows():
-            async for tl_doc in get_timeline_for_repo(app, context, repo_slug, metric):
-                dt = tl_doc['timestamp']
-                dt_str = all_dts.get(dt)
-                if dt_str is None:
-                    dt_str = utils.dt_to_str(dt)
-                    all_dts[dt] = dt_str
-                yield {'t':dt_str, 'y':tl_doc['value']}
-
-        color = next(color_iter)
-        tdata = {
-            'label':'{} Total'.format(repo_slug),
-            'fill':False,
-            'backgroundColor':color,
-            'borderColor':color,
-            'lineTension':0,
-            'spanGaps':True,
-            'hidden':repo_slug in hidden_repos,
-        }
-        if metric == 'count':
-            tdata['label'] = '{} Total'.format(repo_slug)
-        elif metric == 'uniques':
-            tdata['label'] = '{} Uniques'.format(repo_slug)
-        tdata['data'] = [d async for d in build_rows()]
-        return tdata
-
-    repo_iter = get_repos_by_rank(app, context, metric, limit)
-    datasets = [await build_chart_repo_dataset(repo_doc) async for repo_doc in repo_iter]
-    start_dt = min(all_dts.keys())
-    data = {
-        'chart_data':{
-            'datasets':datasets,
-        },
-        'dataset_ids':dataset_ids,
-        'start_datetime':all_dts[start_dt],
-    }
-    return data
-
-async def get_traffic_chart_data(app, context):
-    repos = await get_repos(app, context)
-    hidden_repos = context['hidden_repos']
-    metric = context.get('data_metric', 'count')
-    limit = context.get('limit', 10)
-    return await build_chart_datasets(app, context, metric, limit, hidden_repos)
 
 @aiohttp_jinja2.template('home.html')
 async def home(request):
@@ -239,12 +128,13 @@ async def repo_detail(request):
     repos = await get_repos(request.app, context)
     repo = repos[repo_slug]
     context['repo'] = repo
-    tp = [doc async for doc in get_repo_traffic_paths(request.app, context, repo_slug)]
+    tp = [doc async for doc in chartdata.get_repo_traffic_paths(request.app, context, repo_slug)]
     context['traffic_paths'] = tp
     return context
 
 async def prepare_chart_data_view_context(request):
     context = update_context_dt_range(request)
+    await get_repos(request.app, context)
     repo_slugs = request.query.get('repo_slugs', '')
     if not len(repo_slugs):
         repo_slugs = []
@@ -269,16 +159,16 @@ async def prepare_chart_data_view_context(request):
 
 async def get_traffic_chart_data_json(request):
     context = await prepare_chart_data_view_context(request)
-    chart_data = await get_traffic_chart_data(request.app, context)
+    chart_data = await chartdata.get_traffic_chart_data(request.app, context)
     return web.json_response(chart_data, dumps=utils.jsonfactory.dumps)
 
 async def get_combined_chart_data_json(request):
     context = await prepare_chart_data_view_context(request)
     context['color_iter'] = iter_colors()
     context['data_metric'] = 'count'
-    count_data = await get_traffic_chart_data(request.app, context)
+    count_data = await chartdata.get_traffic_chart_data(request.app, context)
     context['data_metric'] = 'uniques'
-    unique_data = await get_traffic_chart_data(request.app, context)
+    unique_data = await chartdata.get_traffic_chart_data(request.app, context)
     count_data['chart_data']['datasets'].extend(unique_data['chart_data']['datasets'])
     count_data['dataset_ids'].extend(unique_data['dataset_ids'])
     return web.json_response(count_data, dumps=utils.jsonfactory.dumps)
